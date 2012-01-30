a3b6ea2938b37a13104a1cd2dc771d899839b693
Ben Wickerson <benjaminwickerson@yahoo.co.uk>
2012-01-30 22:37:06 +0000

    Make a basic user model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
+  def logo
+  @logo = image_tag("logo.png", alt: "Sample App", class: "round")
+  end
+
 	def full_title(page_title)
-		base_title = "Ruby on Rails Tutorial Sample App"
+		base_title = "Growify"
 		if page_title.empty?
 			base_title
 		else
